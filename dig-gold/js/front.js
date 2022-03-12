d53acac7931d8776dae5b3f3b7089449ac1ebb62
gaoachao <1227448581@qq.com>
2022-03-12 13:34:50 +0800

    day3

---
+++ dig-gold/js/front.js
@@ -0,0 +1,13 @@
+
+/*鼠标点击事件：div显示或者隐藏*/
+function showhidediv(id){  
+  var sbtitle=document.getElementById(id);  
+  if(sbtitle){  
+     if(sbtitle.style.display=='block'){  
+     sbtitle.style.display='none';  
+     }else{  
+     sbtitle.style.display='block';  
+     }  
+  }
+}
+
