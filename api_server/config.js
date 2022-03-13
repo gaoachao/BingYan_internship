8aa276673f68e7629f757fbaccb45a8ebee94b60
gaoachao <1227448581@qq.com>
2022-03-13 23:54:27 +0800

    day4

---
+++ api_server/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //加密和解密 Token 的密钥
+  jwtSecretKey: 'itheima No1',
+  //token 的有效期
+  expiresIn:'10h',
+}
--- api_server/router_handler/user.js
+++ api_server/router_handler/user.js
@@ -1,12 +1,16 @@
 /**
  * 在这里定义和用户相关的路由处理函数，供 /router/user.js 模块进行调用
  */
-
-
 //导入数据库操作模块
  const db = require('../db/index')
  //导入bcryptjs 包
  const bcrypt = require('bcryptjs')
+ //导入生成Token的包
+ const jwt = require('jsonwebtoken')
+//导入全局的配置文件
+const config = require('../config')
+
+
 
 // 注册用户的处理函数
 exports.regUser = (req, res) => {
@@ -69,8 +73,25 @@ exports.login = (req, res) => {
     if (err) return res.cc(err)
     // 执行 SQL 语句成功，但是查询到数据条数不等于 1
     if (results.length !== 1) return res.cc('登录失败！')
+
+
     // TODO：判断用户输入的登录密码是否和数据库中的密码一致
-    res.send('login OK')
+    const compareResult = bcrypt.compareSync(userinfo.password, results[0].password)
+    // 如果对比的结果等于 false, 则证明用户输入的密码错误
+    if (!compareResult) return res.cc('登录失败！')
+
+
+    //TODO:在服务器端生成Token的字符串
+    const user = { ...results[0],password:'',user_pic:''}
+    //对用户信息进行加密生成Token字符串
+    const tokenStr = jwt.sign(user,config.jwtSecretKey,{expiresIn:config.expiresIn})
+    // 调用 res.send()将Token 响应给客户端
+    res.send({
+      status: 0,
+      message: '登录成功！',
+      // 为了方便客户端使用 Token，在服务器端直接拼接上 Bearer 的前缀
+      token: 'Bearer ' + tokenStr,
+    })
   })
   
 }
