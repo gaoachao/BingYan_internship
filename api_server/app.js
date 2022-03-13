7093f1535a86ff6528bca55036a9b257748e607f
gaoachao <1227448581@qq.com>
2022-03-13 20:28:12 +0800

    day4

--- api_server/app.js
+++ api_server/app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 //创建服务器的实例对象
 const app = express()
+const joi = require('@hapi/joi')
 
 // 导入 cors 中间件
 const cors = require('cors')
@@ -11,9 +12,38 @@ app.use(cors())
 //配置解析表单数据的中间件 只能解析application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: false }))
 
-// 导入并注册用户路由模块
+
+// 响应数据的中间件
+// 在路由之前，封装res.cc函数
+app.use(function (req, res, next) {
+    // status = 0 为成功； status = 1 为失败； 默认将 status 的值设置为 1，方便处理失败的情况
+    res.cc = function (err, status = 1) {
+      res.send({
+    // 状态
+        status,
+    // 状态描述，判断 err 是 错误对象 还是 字符串
+        message: err instanceof Error ? err.message : err,
+      })
+    }
+    next()
+  })
+
+
+// 导入并使用用户路由模块
 const userRouter = require('./router/user')
 app.use('/api', userRouter)
+
+
+
+//定义错误级别的中间件
+app.use(function (err, req, res, next) {
+// 数据验证失败
+    if (err instanceof joi.ValidationError)  res.cc(err)
+// 未知错误
+    res.cc(err)
+  })
+
+
 
 //启动服务器
 app.listen(3007,()=>{
---
+++ api_server/router_handler/user.js
@@ -0,0 +1,78 @@
+/**
+ * 在这里定义和用户相关的路由处理函数，供 /router/user.js 模块进行调用
+ */
+
+
+//导入数据库操作模块
+ const db = require('../db/index')
+ //导入bcryptjs 包
+ const bcrypt = require('bcryptjs')
+
+// 注册用户的处理函数
+exports.regUser = (req, res) => {
+  //获取客服端提交到服务器的用户信息
+  const userinfo = req.body
+  //对表单中的数据，进行合法性的校验
+  //if (!userinfo.username || !userinfo.password) {
+  //return res.send({ status: 1, message: '用户名或密码不能为空！' })
+  //}
+
+
+
+
+
+
+
+ //定义 SQL 语句，查询用户名是否被占用
+  const sql = `select * from ev_users where username=?`
+  db.query(sql, [userinfo.username], function (err, results){
+    // 执行 SQL 语句失败
+    if (err) {
+      //return res.send({ status: 1, message: err.message })
+      return res.cc(err)
+    }
+    // 用户名被占用
+    if (results.length > 0) {
+      //return res.send({ status: 1, message: '用户名被占用，请更换其他用户名！' })
+      return res.cc('用户名被占用，请更换其他用户名！')
+    }
+    // 调用bcrypt.hashSync()对密码进行加密
+    userinfo.password = bcrypt.hashSync(userinfo.password,10)
+    //定义插入新用户的SQL语句
+    const sql = 'insert into ev_users set ?'
+    //调用db.query()执行SQL语句
+    db.query(sql, { username: userinfo.username, password: userinfo.password }, function (err, results) {
+    // 执行 SQL 语句失败
+      //if (err) return res.send({ status: 1, message: err.message })
+      if(err) return res.cc(err)
+
+    // SQL 语句执行成功，但影响行数不为 1,也需要return出去
+
+    //if (results.affectedRows !== 1) {return res.send({ status: 1, message: '注册用户失败，请稍后再试！' })}
+    if (results.affectedRows !== 1) return res.cc('注册用户失败，请稍后再试！')
+    //上述的两次判断都没有return出去，那么就注册成功
+    //res.send({ status: 0, message: '注册成功！' })
+    res.cc('注册成功',0)
+    })
+  })
+}
+
+// 登录的处理函数
+exports.login = (req, res) => {
+  //接受表单的数据
+  const userinfo = req.body
+  //定义SQL语句
+  const sql = `select * from ev_users where username=?`
+  //执行SQL语句，根据用户名查询用户信息
+  db.query(sql, userinfo.username, function (err, results) {
+    // 执行 SQL 语句失败
+    if (err) return res.cc(err)
+    // 执行 SQL 语句成功，但是查询到数据条数不等于 1
+    if (results.length !== 1) return res.cc('登录失败！')
+    // TODO：判断用户输入的登录密码是否和数据库中的密码一致
+    res.send('login OK')
+  })
+  
+}
+
+
